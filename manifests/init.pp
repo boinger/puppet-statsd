d4d8b69b1463e2a0c85535b637b77c98414e0527
jeff@jeffvier.com <jeff@jeffvier.com>
2013-10-24 15:22:58 -0700

    split the service def into its own define{} to accommodate multiple listeners

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,17 @@
 class statsd (
   $graphite_host,
   $graphite_port  = 2003,
+  $suffix         = '',
   $bind_interface = '127.0.0.1',
   $port           = 8125,
   $debug          = 1,
   $flush_interval = 5,
   $version        = '0.12'
   ) {
+
+  if (!$suffix) {
+    $suffix = $hostname
+  }
 
   Exec { path => ["/usr/bin", "/bin", "/sbin"], }
   Package { ensure => "installed", }
@@ -30,7 +35,7 @@ class statsd (
     require  => [Package['ruby'], Package['rubygems'], ];
   }
 
-   exec {
+  exec {
     "clone ruby-statsdserver":
       cwd     => "/usr/local/src",
       command => "git clone git://github.com/boinger/ruby-statsdserver.git",
@@ -47,50 +52,26 @@ class statsd (
       cwd     => "/usr/local/src/ruby-statsdserver",
       command => "gem install statsdserver",
       creates => "/usr/bin/statsd",
-      notify  => Exec['restart-statsd'],
       require => [Exec['build ruby-statsdserver'], ];
-
-    "restart-statsd":
-      command     => "stop statsd ; start statsd",
-      refreshonly => true,
-      require     => [ Exec["install ruby-statsdserver"], Service['statsd'], ],
-      subscribe   => [ File['/etc/statsd.conf'], File['/etc/init/statsd.conf'], ];
   }
 
   file {
-    "/etc/init/statsd.conf":
-      source  => "puppet:///modules/statsd/etc/init/statsd.conf",
-      require => User['statsd'];
-
-    "/etc/statsd.conf":
-      content => template("statsd/etc/statsd.conf.erb");
-
     "/data/log/statsd":
       ensure  => directory,
       owner   => "statsd",
       mode    => 0755,
       require => File['/data/log'];
-
-    "/data/log/statsd/statsd.log":
-      ensure  => file,
-      owner   => "statsd",
-      require => File['/data/log/statsd'];
   }
 
-  service {
-    'statsd':
-      ensure     => 'running',
-      hasrestart => true,
-      hasstatus  => true,
-      restart    => "/sbin/restart ${name}",
-      start      => "/sbin/start ${name}",
-      stop       => "/sbin/stop ${name}",
-      status     => "/sbin/status ${name} | grep '/running' 1>/dev/null 2>&1",
-      require    => [
-        File['/etc/statsd.conf'],
-        File['/data/log/statsd/statsd.log'],
-        File['/etc/init/statsd.conf'],
-        Exec['install ruby-statsdserver'],
-        ];
+  statsd::service {
+    "statsd-${port}":
+      graphite_host  => $graphite_host,
+      graphite_port  => $graphite_port,
+      suffix         => $suffix,
+      bind_interface => $bind_interface,
+      listen_port    => $port,
+      debug          => $debug,
+      flush_interval => $flush_interval;
   }
+
 }
---
+++ manifests/service.pp
@@ -0,0 +1,54 @@
+define statsd::service (
+  $graphite_host,
+  $graphite_port  = 2003,
+  $suffix,
+  $bind_interface = '127.0.0.1',
+  $listen_port    = 8125,
+  $debug          = 1,
+  $flush_interval = 5,
+  ) {
+
+  Exec { path => ["/usr/bin", "/bin", "/sbin"], }
+  File {
+    ensure => present,
+    owner  => "root",
+    group  => "root",
+    mode   => 0644,
+  }
+
+  exec {
+    "restart-${name}":
+      command     => "stop ${name} ; start ${name}",
+      refreshonly => true,
+      require     => Service["${name}"],
+      subscribe   => [ File["/etc/${name}.conf"], File["/etc/init/${name}.conf"], ];
+  }
+
+  file {
+    "/etc/init/${name}.conf":
+      content => template("statsd/etc/init/statsd.conf.erb");
+
+    "/etc/${name}.conf":
+      content => template("statsd/etc/statsd.conf.erb");
+
+    "/data/log/statsd/${name}.log":
+      ensure  => file,
+      owner   => "statsd",
+      require => File['/data/log/statsd'];
+  }
+
+  service {
+    $name:
+      ensure     => 'running',
+      hasrestart => true,
+      hasstatus  => true,
+      restart    => "/sbin/restart ${name}",
+      start      => "/sbin/start ${name}",
+      stop       => "/sbin/stop ${name}",
+      status     => "/sbin/status ${name} | grep '/running' 1>/dev/null 2>&1",
+      require    => [
+        File["/etc/init/${name}.conf"],
+        File["/etc/${name}.conf"],
+        ];
+  }
+}
